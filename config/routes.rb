e0ff1e60bd8f5fd55c46ed234eb4a03005769bc6
Matt <mattfsewell@gmail.com>
2021-03-29 10:40:40 -0400

    fixed signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :index]
       get '/login', to: 'users#token_authenticate'
-      post '/login', to: 'auth#create'
+      post '/login', to: 'users#create'
       get '/profile', to: 'users#profile'
     end
   end
