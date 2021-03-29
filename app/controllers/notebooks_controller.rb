ab0e6c455b3ada94cd1706a76f106d23a44209dd
Matt <mattfsewell@gmail.com>
2021-03-29 16:59:18 -0400

    new routes

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,2 +1,25 @@
 class NotebooksController < ApplicationController
+
+    def this_notebook
+        # byebug
+        if current_user.notebook
+            render json: current_user.notebook
+        else
+            render json: {error: "no notebook yet"}
+        end
+    end
+
+    def create
+        # byebug
+        @notebook = Notebook.new(notebook_params)
+        @notebook.user = current_user
+        @notebook.save
+    end
+
+    private
+
+    def notebook_params
+        params.require(:notebook).permit(:title)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   resources :chapters
   resources :notebooks
   get '/chapters/:id/pages', to: 'chapters#this_chapter'
+  get '/users/:id/notebook', to: 'notebooks#this_notebook'
+  post '/users/:id/notebook', to: 'notebooks#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :index]
       get '/login', to: 'users#token_authenticate'
-      post '/login', to: 'users#create'
+      post '/login', to: 'auth#create'
+      post '/signup', to: 'users#create'
       get '/profile', to: 'users#profile'
     end
   end
